f2d499810a956ec68e8496605262a5b0bacf979d
Mikhaila Friske <frisk028@umn.edu>
2019-04-09 18:50:32 -0600

    updating requirements and readme

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # AdaCADWeaver
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.2.4.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.9.0.
+
+To download the requirements run `npm install`.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,9 +15,9 @@ import { AppComponent } from './app.component';
 
 import { CoreModule } from './core/core.module';
 import { WeaverModule } from './weaver/weaver.module';
-import { secrets } from '../environments/secrets';
+// import { secrets } from '../environments/secrets';
 
-export const firebaseConfig = secrets;
+// export const firebaseConfig = secrets;
 
 @NgModule({
   declarations: [
@@ -30,10 +30,10 @@ export const firebaseConfig = secrets;
     HttpModule,
     WeaverModule,
     BrowserAnimationsModule,
-    AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireDatabaseModule,
-    AngularFireAuthModule,
-    AngularFireStorageModule
+    // AngularFireModule.initializeApp(firebaseConfig),
+    // AngularFireDatabaseModule,
+    // AngularFireAuthModule,
+    // AngularFireStorageModule
   ],
   providers: [{ 
     provide: HAMMER_GESTURE_CONFIG, 
