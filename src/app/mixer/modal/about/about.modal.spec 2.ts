b0b48bc157e41b2bc4a3d6bd2903e82c3d4214ae
ergoodwill <emmar.goodwill@gmail.com>
2021-05-22 22:55:52 -0600

    fixing incorrect path for finding AboutModal

--- src/app/mixer/modal/about/about.modal.spec 2.ts
+++ src/app/mixer/modal/about/about.modal.spec 2.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AboutModal } from './about.component';
+import { AboutModal } from './about.modal';
 
 describe('AboutModal', () => {
   let about: AboutModal;
