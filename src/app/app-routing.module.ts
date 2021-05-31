2857c1ca5b125e3ccfced9eb157196bbc04c07a6
Devendork <ldevendorf@gmail.com>
2021-05-31 10:32:06 -0600

    added updates to readme about new structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,6 @@ import { WeaverComponent } from './weaver/weaver.component';
 import { MixerComponent } from './mixer/mixer.component';
 
 
-
-/**
- * Use this to specify if you want to load the weaver application or the mixer application. 
- * enable this by changing "component" below to "WeaverComponent" or "MixerComponent"
- */
 const routes: Routes = [
   {
     path: 'mixer',
