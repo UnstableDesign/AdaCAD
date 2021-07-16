b08a800af3227157499b6ff7086b47b2308c86f0
ergoodwill <emmar.goodwill@gmail.com>
2021-07-16 17:57:07 -0600

    Start of VAE integration (including blueprint of service for weights of models)

---
+++ src/app/weaver/learning/vae.ts
@@ -0,0 +1,11 @@
+import * as tf from '@tensorflow/tfjs'
+
+export class VAE {
+    epsilons: any = [];
+    latent_dim: number = 3;
+    batch_size: number = 16;
+
+    generateFromSeed(draft) {
+        
+    }
+}
