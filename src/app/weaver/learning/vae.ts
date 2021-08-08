74c8d276e84bbb831fd15db0e631ae8e4e405031
ergoodwill <emmar.goodwill@gmail.com>
2021-08-08 16:09:43 -0600

    Deleting unused custom layer

--- src/app/weaver/learning/vae.ts
+++ src/app/weaver/learning/vae.ts
@@ -1,31 +1,5 @@
 import * as tf from '@tensorflow/tfjs'
 
-class lambdaLayer extends tf.layers.Layer {
-    private shape;
-    private latent_dim;
-    constructor(shape, latent_dim) {
-        super();
-        this.shape = shape;
-        this.latent_dim = latent_dim;
-    }
-
-    call(inputs) {
-        let z_mean = inputs[0];
-        let z_log_var = inputs[1];
-        let epsilon = tf.randomNormal([this.shape[0], this.latent_dim], 0., 1.);
-
-        let toRet = z_mean + tf.matMul(tf.exp(z_log_var), epsilon);
-        return toRet;
-    }
-
-    getConfig() {
-        let config = super.getConfig();
-        config['shape'] = this.shape;
-        config['latent_dim'] = this.latent_dim;
-        return config;
-    }
-}
-
 export class VAE {
     private epsilons: any = [];
     private latent_dim: number = 3;
