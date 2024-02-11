dafa401d4236e5c98bf500d4a7b5eb1458924a3b
Devendork <ldevendorf@gmail.com>
2024-02-10 18:02:36 -0700

    removed mixer design, ops and quick ops for non-use

--- src/app/mixer/mixer.module.ts
+++ src/app/mixer/mixer.module.ts
@@ -8,15 +8,12 @@ import { SnackbarComponent } from './palette/snackbar/snackbar.component';
 import { MarqueeComponent } from './palette/marquee/marquee.component';
 import { OperationComponent } from './palette/operation/operation.component';
 import { ConnectionComponent } from './palette/connection/connection.component';
-import { OpsComponent } from './ops/ops.component';
 import { OpHelpModal } from './modal/ophelp/ophelp.modal';
 import { ImageComponent } from './palette/image/image.component';
 import { EditorModule } from '../editor/editor.module';
 import { NoteComponent } from './palette/note/note.component';
 import { ParameterComponent } from './palette/operation/parameter/parameter.component';
 import { InletComponent } from './palette/operation/inlet/inlet.component';
-import { DesignComponent } from './design/design.component';
-import { QuickopComponent } from './quickop/quickop.component';
 import { DraftrenderingComponent } from './palette/draftrendering/draftrendering.component';
 
 
@@ -35,14 +32,11 @@ import { DraftrenderingComponent } from './palette/draftrendering/draftrendering
         MarqueeComponent,
         OperationComponent,
         ConnectionComponent,
-        OpsComponent,
         OpHelpModal,
         ImageComponent,
         NoteComponent,
         ParameterComponent,
         InletComponent,
-        DesignComponent,
-        QuickopComponent,
         DraftrenderingComponent
         ],
     exports: [
