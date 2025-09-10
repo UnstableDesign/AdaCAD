fddd036805e00ad322a546ccb79a170171e74390
Devendork <ldevendorf@gmail.com>
2025-09-10 12:41:39 -0600

    added quick simulation test to make sure wefts are generated

--- tests/simulation/simulation.test.js
+++ tests/simulation/simulation.test.js
@@ -1,16 +1,32 @@
 
 import { call } from '../../src/operations';
 import { satin } from '../../src/operations';
+import { createMaterial } from '../../src/material';
+import { getDraftTopology } from '../../src/simulation/simulation';
 
-const computeSimulationData = require('../../src/simulation/simulation').computeSimulationData;
+const followTheWefts = require('../../src/simulation/simulation').followTheWefts;
 
-test('testing compute simdata', () => {
+test('testing compute simdata', async () => {
 
-    const satin_draft = call(satin, [5, 2, false]).then(
-        dat => {
-            // let data = computeSimulationData(satin_draft)
-        }
-    );
+    const material_a = createMaterial({ id: 0 })
+    const material_b = createMaterial({ id: 1 })
+
+    const simVars = {
+        pack: 10,
+        lift_limit: 10,
+        use_layers: false,
+        warp_spacing: 5,
+        layer_spacing: 5,
+        wefts_as_written: false,
+        simulate: false,
+        radius: 5,
+        ms: [material_a, material_b]
+    }
+
+    const draft_list = await call(satin, [5, 2, false]);
+    const topo = await getDraftTopology(draft_list[0], simVars);
+    const wefts = await followTheWefts(draft_list[0], topo, simVars)
+    expect(wefts.vtxs).not.toEqual([])
 
 })
 
