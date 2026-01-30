c307032298db6df885e4f830099036d329f281b5
Devendork <ldevendorf@gmail.com>
2026-01-30 08:47:28 -0700

    removed ui code

--- projects/ui/src/app/core/events.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EventsDirective } from './events.directive';
-
-describe('EventsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new EventsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
